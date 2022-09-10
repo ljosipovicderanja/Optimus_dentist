0ba2bb013a221963c0d6ead4fa113d2cf636c471
Lucija <lucijaderanja@gmail.com>
2022-09-10 13:58:50 +0200

    Added new api

--- server/routes/api/post.js
+++ server/routes/api/post.js
@@ -6,8 +6,29 @@ const router = express.Router();
 
 //get posts (Read)
 router.get("/test", async (req, res) => {
-  let collection = await loadCollection("users");
+  let collection = await loadCollection("newCollection");
   res.send(await collection.find({}).toArray());
+});
+
+//add post
+router.post("/test", async (req, res) => {
+  let db = await connect();
+  let data = {
+    name: "name",
+    surname: "surname",
+    date: Date.now(),
+  };
+
+  await db.collection("newCollection").insertOne(data);
+
+  res.status(201).send();
+});
+
+//delete post
+router.delete("/:id", async (req, res) => {
+  let db = await loadCollection("newCollection");
+  await db.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
+  res.status(200).send();
 });
 
 async function loadCollection(collectionName) {
