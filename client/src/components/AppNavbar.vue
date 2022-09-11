d0296d755484239dda94ad9c24ffe7309be21c71
Lucija <lucijaderanja@gmail.com>
2022-09-11 12:50:12 +0200

    Added vue screens and routes

--- client/src/components/AppNavbar.vue
+++ client/src/components/AppNavbar.vue
@@ -9,16 +9,19 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item">
-            <router-link class="nav-link" to="/">Login</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/about">Register</router-link>
-          </li>
-          <li class="nav-item">
             <router-link class="nav-link" to="/">Home</router-link>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" to="/about">Add dentist</router-link>
+            <router-link class="nav-link" to="/login">Login</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/register">Register</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/findDentist">Find dentist</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/addDentist">Add dentist</router-link>
           </li>
         </ul>
       </div>
---
+++ client/src/views/FindDentist.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="about">
+    <h1>This is an FindDentist page</h1>
+    <button @click="testBaza()">test button</button>
+    <div id="items" v-for="item in items" v-bind:key="item._id">
+      <p>{{ item._id }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { db } from "@/services/index.js";
+
+export default {
+  name: "FindDentist",
+  components: {},
+
+  data() {
+    return {
+      items: [],
+    };
+  },
+
+  async created() {
+    this.items = await db.getAllItemsFromCollectionMDb("test");
+  },
+
+  methods: {
+    async testBaza() {
+      //should use await
+      return await db.getAllItemsFromCollectionMDb("test");
+    },
+  },
+};
+</script>
